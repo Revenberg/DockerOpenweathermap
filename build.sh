b3ab20de851de847a8c4fd8aefaaab3b4328ec50
Sander Revenberg <sander@revenberg.info>
2021-08-04 17:09:19 +0200

    .

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if $(git remote show origin | grep "local out of date" | wc -l) > 0:
+if ($(git remote show origin | grep "local out of date" | wc -l) -eq 0); then
   git pull
   chmod +x build.sh
 
   docker image build -t revenberg/openweathermap .
 
   docker push revenberg/openweathermap
+fi
